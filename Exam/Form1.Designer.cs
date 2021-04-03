614bd37b96d97499d65ce0edbbb2caf29e8fc6f0
PavlovStanislav <73657304+PavlovStanislav@users.noreply.github.com>
2021-04-04 02:29:49 +0300

    Add files via upload

---
+++ Exam/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+using System;
+
+namespace Exam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.eXITToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(447, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(312, 381);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 61);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add new user";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(160, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 61);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Select user by id";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(160, 69);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 61);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Delete user";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(32, 168);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 61);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Select all";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(296, 69);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 61);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Update user info by id";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 349);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(203, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(32, 392);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(203, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(32, 305);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(203, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(135, 262);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eXITToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // eXITToolStripMenuItem
+            // 
+            this.eXITToolStripMenuItem.Name = "eXITToolStripMenuItem";
+            this.eXITToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.eXITToolStripMenuItem.Text = "EXIT";
+            this.eXITToolStripMenuItem.Click += new System.EventHandler(this.eXITToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem eXITToolStripMenuItem;
+    }
+}
+
---
+++ Exam/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+
+namespace Exam
+{
+    public partial class Form1 : Form
+    {
+        private OleDbCommand run;
+        private OleDbConnection connect;
+        private OleDbDataReader read;
+        private string connection_string = @"Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;
+                                                        User ID=01\325;Initial Catalog=MyDb;Data Source=01\SQLEXPRESS";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //Add new user
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OleDbConnection connect = new OleDbConnection(connection_string))
+            {
+                string username = textBox1.Text;
+                string password = textBox2.Text;
+                string email = textBox3.Text;
+                string user_ID = textBox4.Text;
+
+                connect.Open();
+                
+                run = new OleDbCommand("INSERT INTO [MyDb].[db_owner].[Users] VALUES ('"+ user_ID +"'," +
+                    " '" + username + "'," +
+                    " '" + password + "'," +
+                    " '" + email + "')", connect);
+
+                run.Parameters.Add(new OleDbParameter("user_id", textBox4.Text));
+                run.Parameters.Add(new OleDbParameter("username", textBox1.Text));
+                run.Parameters.Add(new OleDbParameter("password", textBox2.Text));
+                run.Parameters.Add(new OleDbParameter("email", textBox3.Text));
+
+                run.ExecuteNonQuery();
+            }
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+
+            Select_all_users();
+        }
+
+        //Select all users
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Select_all_users();
+        }
+
+        private void Select_all_users()
+        {
+            using (connect = new OleDbConnection(connection_string))
+            {
+                connect.Open();
+
+                textBox1.Text = string.Empty;
+                textBox2.Text = string.Empty;
+                textBox3.Text = string.Empty;
+                textBox4.Text = string.Empty;
+
+                run = new OleDbCommand("SELECT [user_id], [username], [password], [email] FROM [MyDb].[db_owner].[Users]", connect);
+
+                read = run.ExecuteReader();
+
+                while (read.Read())
+                {
+                    listBox1.Items.Add(read.GetValue(0) + " " + read.GetString(1));
+                }
+            }
+        }
+
+        //Delete user by user id
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (connect = new OleDbConnection(connection_string))
+            {
+                textBox1.Text = string.Empty;
+                textBox2.Text = string.Empty;
+                textBox3.Text = string.Empty;
+                string user_ID = textBox4.Text;
+
+                connect.Open();
+
+                run = new OleDbCommand("DELETE FROM [MyDb].[db_owner].[Users] WHERE [user_id] = " + user_ID, connect);
+                run.Parameters.Add(new OleDbParameter("user_ID", textBox4.Text));
+
+                run.ExecuteNonQuery();
+            }
+            textBox4.Text = string.Empty;
+
+            Select_all_users();
+        }
+
+        //Select user by user id
+            
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (connect = new OleDbConnection(connection_string))
+            {
+                string username = textBox1.Text;
+                string password = textBox2.Text;
+                string email = textBox3.Text;
+                string user_ID = textBox4.Text;
+
+                connect.Open();
+
+                run = new OleDbCommand("SELECT * FROM [MyDb].[db_owner].[Users] WHERE [user_id] = " + user_ID, connect);
+                run.Parameters.Add(new OleDbParameter("user_ID", textBox4.Text));
+
+                read = run.ExecuteReader();
+
+                read.Read();
+
+                textBox1.Text = read["username"].ToString();
+                textBox2.Text = read["password"].ToString();
+                textBox3.Text = read["email"].ToString();
+            }
+        }
+
+        //Update user info by id
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using (connect = new OleDbConnection(connection_string))
+            {
+                string username = textBox1.Text;
+                string password = textBox2.Text;
+                string email = textBox3.Text;
+                string user_ID = textBox4.Text;
+
+                connect.Open();
+                
+                run = new OleDbCommand("UPDATE [MyDb].[db_owner].[Users] SET [username] = '" + username + "'," +
+                    " [password] = '" + password + "'," +
+                    " [email] = '" + email + "'" +
+                    " WHERE [user_id] = " + user_ID, connect);
+
+
+                run.Parameters.Add(new OleDbParameter("username", textBox1.Text));
+                run.Parameters.Add(new OleDbParameter("password", textBox2.Text));
+                run.Parameters.Add(new OleDbParameter("email", textBox3.Text));
+                run.Parameters.Add(new OleDbParameter("user_ID", textBox4.Text));
+
+                run.ExecuteNonQuery();   
+            }
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+
+            Select_all_users();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = " input username";
+            textBox2.Text = " input password";
+            textBox3.Text = " input e-mail";
+            textBox4.Text = " input ID";
+        }
+
+        private void eXITToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            connect.Close();
+            this.Close();
+        }
+    }
+}
